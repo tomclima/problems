3a552abcc7015cd1cc1c57d2e06c69b6084f49c2
Tome Lima <tcl@cin.ufpe.br>
2024-11-29 00:38:16 -0300

    progresso queens

--- progcomp/L0/d - queens/solution.cpp
+++ progcomp/L0/d - queens/solution.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-bool safe_diagonal(vector<string> &bstate, pair<int, int> &atk, pair<int, int> &def){
+
+int count = 0;
+
+bool safe_diagonal(std :: vector<std :: string> &bstate, std :: pair<int, int> &atk, std :: pair<int, int> &def){
     int b0 = atk.first - atk.second;
     int b1 = atk.first + atk.second;
     int answer = true;
@@ -13,44 +15,81 @@ bool safe_diagonal(vector<string> &bstate, pair<int, int> &atk, pair<int, int> &
     return answer;
 }
 
-bool safe(vector<string> &bstate, vector<pair<int,int>> &qvec, pair<int, int> qpos){
+bool safe(std :: vector<std :: string> &bstate, std :: vector<std :: pair<int,int>> &qvec, int qn){
     bool answer = true;
-    for(int i = 0; i < qvec.size(); i++){
-        if(qvec[i].first > 0 and qvec[i].first == qpos.first){
+
+    std :: pair<int, int> qpos = qvec[qn];
+    
+    if(bstate[qpos.first][qpos.second] == '*'){
+        answer = false;
+    }
+
+    for(int i = 0; i < qn; i++){
+        if(qvec[i].first == qpos.first){
             answer = false;
         }
-        else if(qvec[i].first > 0 and qvec[i].second == qpos.second){
+        else if(qvec[i].second == qpos.second){
             answer = false;
         }
-        else if(qvec[i].first > 0 and !safe_diagonal(bstate, qvec[i], qpos)){
+        else if(!safe_diagonal(bstate, qvec[i], qpos)){
             answer = false;
+        }
+        if(answer == false){
+            break;
         }
     }
+
+
 
     return answer;
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
+void three_safe(std :: vector<std :: string> &board, std :: vector<std :: pair<int, int>> &qvec, int qn){
+    while(qvec[qn].first < board.size()){
+        while(qvec[qn].second < board.size()){
+            if(safe(board, qvec, qn)){
+                if(qn == 2){
+                    count++;
+                }
+            
+                else{
+                    qvec[qn+1].first = qvec[qn].first + 1;
+                    qvec[qn+1].second = 0;
+                    three_safe(board, qvec, qn+1);
+                }
+            }
+            qvec[qn].second++;   
+        }
+        qvec[qn].second = 0;
+        qvec[qn].first++;
+    }
 
-    vector<string> board;
-    vector<pair<int, int>> qvec;
+    return;
+}
+
+int main(){
+    std :: ios_base::sync_with_stdio(false);
+
+    std :: vector<std :: string> board;
+    std :: vector<std :: pair<int, int>> qvec;
 
     for(int i = 0; i < 8; i ++){
-        string line;
-        cin >> line;
+        std :: string line;
+        std :: cin >> line;
         board.push_back(line);
     }
 
     for(int i = 0; i < 3; i++){
-        pair<int, int> q;
-        q.first = -1;
-        q.second = -1;
+        std :: pair<int, int> q;
+        q.first = 0;
+        q.second = 0;
         qvec.push_back(q);
     }
 
     
+    three_safe(board, qvec, 0);
 
+    std :: cout << count << std :: endl;
 
 
 }
