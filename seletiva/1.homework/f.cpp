15628ef63da915617a4760fbb339f0553615214d
tcl <tomecostalima1228@gmail.com>
2024-12-02 12:02:43 -0300

    midway f.cpp

---
+++ seletiva/1.homework/f.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define lng long long int
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<lng> pl;
+
+    for(int i = 0; i < n; i++){
+        int k;
+        pl.push_back(k);
+    }
+
+    set<pair<int, int>, less<pair<int, int>>
+}
