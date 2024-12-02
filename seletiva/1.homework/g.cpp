2ee564db73f6fb17eb931dc49e855d14211fdd19
tcl <tomecostalima1228@gmail.com>
2024-12-02 11:31:20 -0300

    better solution

--- seletiva/1.homework/g.cpp
+++ seletiva/1.homework/g.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 #define lng long long
+#define heap priority_queue
 
 int main(){
     std :: ios_base::sync_with_stdio(false);
@@ -19,15 +20,17 @@ int main(){
     }
     prefix.push_back(sum);
 
-    std :: priority_queue<lng int, std :: vector<lng int>, std :: greater<lng int>> min;
-    min.push(prefix[0]);
+    int min;
+    min = prefix[0];
     lng int best = prefix[1] - prefix[0];
     for(int r = 0; r < n; r++){
-        lng int sub = prefix[r+1] - min.top(); 
+        lng int sub = prefix[r+1] - min; 
         if(best < sub){
             best = sub;
         }
-        min.push(prefix[r + 1]);
+        if(prefix[r+1] < min){
+            min = prefix[r+1];
+        };
     } 
 
     std :: cout << best;
