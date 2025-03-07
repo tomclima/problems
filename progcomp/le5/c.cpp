1ab0f50547ee61b3edffaa2645166e52b5993181
tcl <tomecostalima1228@gmail.vom>
2025-03-07 18:36:47 -0300

    kmp solved

--- progcomp/le5/c.cpp
+++ progcomp/le5/c.cpp
@@ -9,26 +9,55 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    string s;
-    string pat;
+    string s, pat;
     cin >> s >> pat;
-
-    int d =  5;
-    int q = 1e9+7;
-
     int n = s.length();
     int m = pat.length();
+    
+    vector<int> lps;
+    lps.resize(m);
+    
+    // lps generation
+    int l = 0;
+    int i = 1;
+    while(i < m){
+        if(pat[l] == pat[i]){
+            lps[i] = l+1;
+            l++;
+            i++;
+        }
+        else if (l == 0){
+            lps[i] = 0;
+            i++;
+        }
+        else{
+            l = lps[l-1];
+        }
+    }
 
-    int c = 1;
-    for(int i = 0; i < m -1; i++){
-        c = c * d % q;
+    // kmp algorithm
+    set<int> pos;
+    i = 0;
+    int j = 0;
+
+    while(i < n){
+        if(s[i] == pat[j]){
+            i++;
+            j++;
+        }
+        if(j == m){
+            pos.insert(i - j);
+            j = lps[j -1];
+        }
+        else if(i < n and s[i] != pat[j]){
+            if(j == 0) i++;
+            else j =lps[j - 1];
+        }
     }
-    int p = 0;
-    int t = 0;
-    for(int i = 0; i < m-1; i++){
-        p = (p * d + pat[i]) % q;
-        t = (t * d + s[i]) % q;
-    }
+
+    
+    cout << pos.size() << endl;
+
     
 
 }
