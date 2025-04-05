d2e219ed0bf3f617710170a099eb0d51cf2e7a60
tomclima <tomecostalima1228@gmail.com>
2025-04-05 13:07:28 -0300

    start of botup

--- progcomp/l7/a.cpp
+++ progcomp/l7/a.cpp
@@ -20,81 +20,57 @@ using namespace std;
 class State{
     
     public:
-        lng min_time;
-        char orientation;
-
-        State(){
-            min_time = INF;
-            orientation = NORTH;
-        }
+    lng min_time;
+    char orientation;
+    
+    State(){
+        min_time = INF;
+        orientation = NORTH;
+    }
 };
+
+int next_color(int color) {return (color + 1) % N_COLORS;}
+
+vector<vector<vector<State>>> dp;
+
+bool operator>(const vector<int> &a, const vector<int> &b){
+    if(dp[a[0]][a[1]][a[2]].min_time < dp[b[0]][b[1]][b[2]].min_time) return true;
+    return false;
+}
 
 vector<int> colors = {GREEN, WHITE, BLUE, RED, BLACK};
 State ERROR;
 
-State min_time(vector<string> &space, vector<vector<vector<State>>> &dp, vector<vector<vector<bool>>> &visited, int x, int y, int color){
+State min_time(vector<string> &space, vector<vector<vector<bool>>> &visited, int x_origin, int y_origin, int color_origin){
 
-    if(x == 1 and y == 1 and color == 1) {
-        cout << "here";
-    }
+    priority_queue<vector<int>> heap;
 
-    if(dp[x][y][color].min_time < INF) return dp[x][y][color];
-    if(visited[x][y][color]) return ERROR; 
+    heap.push({x_origin, y_origin, color_origin});
 
-    visited[x][y][color] = true;
-
-    lng q = dp[x][y][color].min_time;
-    int orientation = NORTH;
-
-    if(x + 1 < dp.size() and space[x+1][y] != '#') {
+    for(int i = 0; i < space[0].size() * space.size(); i++){
         
-        State recursive = min_time(space, dp, visited, x + 1, y, (N_COLORS +(color - 1) % N_COLORS) % N_COLORS);
-        if(recursive.min_time + abs(NORTH - recursive.orientation) + 1 < q) {
+        vector<int> a;
+        int x;
+        int y;
+        int color;
+        do{
             
-            q = abs(NORTH - recursive.orientation)  + recursive.min_time + 1;
-            orientation = NORTH;
+            if(heap.empty()) return;
+            a = heap.top();
+            heap.pop();
 
+        } while(visited[a[0]][a[1]][a[2]]); 
+        
+        visited[a[0]][a[1]][a[2]] = true;
+        
+        if(x > 0 and !visited[x -1][y][next_color(color)] and dp[x -1][y][next_color(color)] > ){ // going NORTH
+
+            
         }
     }
 
-    if(x - 1 >= 0 and space[x-1][y] != '#') {
-        
-        State recursive = min_time(space, dp, visited, x - 1, y, (N_COLORS +(color - 1) % N_COLORS) % N_COLORS);
-        if(recursive.min_time + abs(SOUTH - recursive.orientation) + 1 < q) {
-            
-            q = abs(SOUTH - recursive.orientation)  + recursive.min_time + 1;
-            orientation = SOUTH;
 
-        }
-    }
 
-    if(y - 1 >= 0 and space[x][y - 1] != '#') {
-        
-        State recursive = min_time(space, dp, visited, x, y -1 , (N_COLORS +(color - 1) % N_COLORS) % N_COLORS);
-        if(recursive.min_time + abs(EAST - recursive.orientation) + 1 < q) {
-            
-            q = abs(EAST - recursive.orientation)  + recursive.min_time + 1;
-            orientation = EAST;
-
-        }
-    }
-
-    if(y + 1 < dp[x].size() and space[x][y +1] != '#') {
-        
-        State recursive = min_time(space, dp, visited, x, y + 1  , (N_COLORS +(color - 1) % N_COLORS) % N_COLORS);
-        if(recursive.min_time + abs(WEST - recursive.orientation) + 1 < q) {
-            
-            q = abs(WEST - recursive.orientation)  + recursive.min_time  + 1;
-            orientation = WEST;
-
-        }
-    }
-
-    visited[x][y][color] = false;
-    dp[x][y][color].min_time = q;
-    dp[x][y][color].orientation = orientation;
-
-    return dp[x][y][color];
 }
 
 int main(){
@@ -112,7 +88,6 @@ int main(){
 
         pair<int, int> S, T;
         
-        vector<vector<vector<State>>> dp;
         vector<vector<vector<bool>>> visited;
 
         visited.resize(m);
@@ -140,7 +115,7 @@ int main(){
         }
 
         dp[S.first][S.second][GREEN].min_time = 0;
-        int ans = min_time(space, dp, visited, T.first, T.second, GREEN).min_time;
+        int ans = min_time(space, visited, T.first, T.second, GREEN).min_time;
         
 
         cout << "Case #" << counter << endl; 
