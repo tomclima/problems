d06e09353804de8cffb173fd7422b3b4632efc83
tomclima <tomecostalima1228@gmail.com>
2025-04-07 00:01:13 -0300

    DEUS EXISTE PRA CARALHO

--- progcomp/l7/a.cpp
+++ progcomp/l7/a.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 #define endl '\n'
 #define lng long long int
-#define INF 1000000000LL
+#define INF 99999999
 #define N_COLORS 5
 #define GREEN 0
 #define WHITE 1
@@ -17,99 +17,37 @@ using namespace std;
 #define SOUTH 2
 #define WEST 3
 
+#define p_queue queue<pair<lng, State>>
+
+vector<int> dy = {0, 1, 0, -1};
+vector<int> dx = {-1, 0, 1, 0};
+
+
+typedef tuple<int, int, int, int> State;
+class Custom_comp{
+    public:
+        bool operator()(const pair<lng, State> &a, const pair<lng, State> &b){
+            return a.first > b.first;
+        }
+};
 
 
 
 
 int next_color(int color) {return (color + 1) % N_COLORS;}
 
-vector<vector<vector<vector<lng>>>> dp;
-
-struct CustomCompare {
-    bool operator()(const vector<lng> &a, const vector<lng> &b){
-        if(a[4] > b[4]) return true;
-        return false;
-    }
-};
-
-
-
-vector<int> colors = {GREEN, WHITE, BLUE, RED, BLACK};
-
-void relax(int x1, int y1, int color1, int ori1, int x2, int y2, int color2, priority_queue<vector<lng>, vector<vector<lng>>, CustomCompare> &heap){
-    if(dp[x2][y2][color2][ori1] > dp[x1][y1][color1][ori1] + 1){
-        dp[x2][y2][color2][ori1] = dp[x1][y1][color1][ori1] + 1;
-        heap.push({x2, y2, color2, ori1, dp[x2][y2][color2][ori1]});
-    }
+int cycle_ori(int orientation, int cycles){
+    return (orientation + cycles) % N_ORIENTATIONS;
 }
 
-void relax_orientations(int x, int y, int color, int orientation, priority_queue<vector<lng>, vector<vector<lng>>, CustomCompare> &heap){
-    
-    int i;
-    i = 1;
-        if(dp[x][y][color][(orientation + i) % N_ORIENTATIONS] > dp[x][y][color][orientation] + 1){
-            dp[x][y][color][(orientation + i) % N_ORIENTATIONS] = dp[x][y][color][orientation] + 1;
-            heap.push({x, y, color, (orientation + i) % N_ORIENTATIONS, dp[x][y][color][(orientation + i) % N_ORIENTATIONS]});
-        }
-
-    i = 3;
-        if(dp[x][y][color][(orientation + i) % N_ORIENTATIONS] > dp[x][y][color][orientation] + 1){
-            dp[x][y][color][(orientation + i) % N_ORIENTATIONS] = dp[x][y][color][orientation] + 1;
-            heap.push({x, y, color, (orientation + i) % N_ORIENTATIONS, dp[x][y][color][(orientation + i) % N_ORIENTATIONS]});
-        }
-
-}
-
-
-void min_time(vector<string> &space, vector<vector<vector<vector<bool>>>> &visited, int x_origin, int y_origin, int color_origin, int orient_origin, int x_dest, int y_dest){
-
-    priority_queue<vector<lng>, vector<vector<lng>>, CustomCompare> heap;
-    
-    heap.push({x_origin, y_origin, color_origin, orient_origin, 0});
-
-    while(!heap.empty()){
-        
-        vector<lng> a;
-        int x;
-        int y;
-        int color;
-        int orientation;
-        do{
-            
-            if(heap.empty()) return;
-            a = heap.top();
-            heap.pop();
-
-            x = a[0]; y = a[1]; color = a[2]; orientation = a[3];
-
-        } while(visited[x][y][color][orientation]); 
-        
-        visited[x][y][color][orientation] = true;
-        relax_orientations(x, y, color, orientation, heap );
-        int next = next_color(color);
-
-        
-
-
-        if(x == x_dest and y == y_dest and color == GREEN) return;
-        if(orientation == NORTH and x > 0 and space[x - 1][y] != '#' and !visited[x-1][y][next][NORTH]){ // GOING NORTH
-            relax(x, y, color, orientation, x-1, y, next, heap);
-        }
-        
-        if( orientation ==  SOUTH and x + 1 < dp.size() and space[x + 1][y] != '#' and !visited[x+1][y][next][SOUTH]){ // GOING SOUTH
-            relax(x, y, color, orientation, x+1, y, next, heap);
-        }
-
-        if(orientation == EAST and y + 1 < dp[0].size() and space[x][y + 1] != '#' and !visited[x][y+1][next][EAST]){ // GOING EAST
-            relax(x, y, color, orientation, x, y+1, next, heap);
-        }
-
-        if(orientation == WEST and y > 0 and space[x][y - 1] != '#' and !visited[x][y - 1][next][WEST]){ // GOING WEST
-            relax(x, y, color, orientation, x, y - 1, next, heap);
-        }
-    }
+void relax(State a, State b, map<State, lng> &dist, p_queue &fila){
+    dist[b] = dist[a] + 1;
+    fila.push({dist[b], b});
     
 }
+
+
+
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -122,56 +60,87 @@ int main(){
         counter++;
         if(m == 0) break;
         if(counter != 1){
-            cout << endl << endl;
+            cout << endl;
         }
         
         vector<string> space;
         
         pair<int, int> S, T;
         
-        vector<vector<vector<vector<bool>>>> visited;
-        
-        
-        visited.resize(m);
-        dp.clear();
-        dp.resize(m);
-
         
         for(int i = 0; i < m; i++){
             
             string line;
             cin >> line;
             space.push_back(line);
-            dp[i].resize(n);
-            visited[i].resize(n);
             
             for(int j = 0; j < n; j++) {
                 
                 if(space[i][j] == 'S') S = {i, j};
                 else if(space[i][j] == 'T') T = {i, j};
-                
-                dp[i][j].resize(N_COLORS);
-                visited[i][j].resize(N_COLORS);
-                for(int k = 0; k < N_COLORS; k++){
-
-                    dp[i][j][k].resize(N_ORIENTATIONS);
-                    visited[i][j][k].resize(N_ORIENTATIONS);
-                    fill(dp[i][j][k].begin(), dp[i][j][k].end(), INF);
-                    fill(visited[i][j][k].begin(), visited[i][j][k].end(), false);
-                }
             }
             
         }
         
-        dp[S.first][S.second][GREEN][NORTH] = 0;
-        min_time(space, visited, S.first, S.second, GREEN, NORTH, T.first, T.second);
+        map<State, lng> dist;
+        map<State, bool> visited;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                for(int k = 0; k < N_COLORS; k++){
+                    for(int l = 0; l < N_ORIENTATIONS; l++){
+                        dist[{i, j, k, l}] = INF;
+                        visited[{i, j, k, l}] = false;
+                    }
+                }
+            }
+        }
 
-        lng ans = min(min(min(dp[T.first][T.second][GREEN][EAST], dp[T.first][T.second][GREEN][WEST]), dp[T.first][T.second][GREEN][SOUTH]), dp[T.first][T.second][GREEN][NORTH]);
+        dist[{S.first, S.second, GREEN, NORTH}] = 0;
+        
+        p_queue fila;
+        
+        fila.push({0, {S.first, S.second, GREEN, NORTH}});
+        while(!fila.empty()){
+            
 
+            State state = fila.front().second;
+            fila.pop(); 
+            if(visited[state] == true) continue;
+            visited[state] = true;
+            
+            int x, y, color, orientation;
+            x =             get<0>(state);
+            y =             get<1>(state);
+            color =         get<2>(state);
+            orientation=    get<3>(state);
+
+            if(x == T.first and y == T.second and color == GREEN) continue;
+            
+            
+            
+            
+            int new_color = next_color(color);
+            
+            int new_x = x + dx[orientation];
+            int new_y = y + dy[orientation];
+            
+            
+            if(!(new_x < 0 or new_x >= space.size() or new_y < 0 or new_y >= space[0].size() or space[new_x][new_y] == '#' or visited[{new_x, new_y, new_color, orientation}])){
+                relax(state, {new_x, new_y, new_color, orientation}, dist, fila);
+            }
+
+            relax(state, {x, y, color, cycle_ori(orientation, 3)}, dist, fila); // relax left
+            relax(state, {x, y, color, cycle_ori(orientation, 1)}, dist, fila); //  relax right
+            
+        }
+        
+        lng ans = INF;
+        for(int i = 0; i < 4; i++) ans = min(ans, dist[{T.first, T.second, GREEN, i}]);
         
         cout << "Case #" << counter << endl; 
-        if(ans < INF) cout << "minimum time = " << ans << " sec";
-        else cout << "destination not reachable";
+        if(ans < INF) cout << "minimum time = " << ans << " sec" << endl;
+        else cout << "destination not reachable" << endl;
         
         
     }
