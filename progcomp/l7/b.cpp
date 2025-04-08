f24404e618e46b35831c2bef17e1ea955a62fbfa
tomclima <tomecostalima1228@gmail.com>
2025-04-07 21:43:21 -0300

    GUERREIRÍSSIMO

--- progcomp/l7/b.cpp
+++ progcomp/l7/b.cpp
@@ -10,29 +10,41 @@ using namespace std;
 typedef tuple<int, int, int> State;
 
 
-vector<int> dn10 = {-1, -1, 0, 0, 0};
-vector<int> dn5 = {0, 0, -2, -1, 0};
+vector<int> dn1 =       {-3, -3, +2, +2};
+vector<int> dn5 =       {-1, +1, -2, +0};
+vector<int> dn10 =      {+0, -1, +0, -1};
+
+vector<int> w_vec =     {+4, +4, +2, +1};
 
 
-lng cost[101][51][151];
+lng cost[150][100][150];
 
-lng solve(State origin, lng total_money, int c_max){
+lng solve(State origin, int n1, int c_max){
     
     int n5 = get<0>(origin);
     int n10 = get<1>(origin);
     int c = get<2>(origin);
-    int ch = get<3>(origin);
-    
-    int n1 = total_money - 8*c - 5*n5 - 10*n10 + ch * 2;
 
-    if(cost[n5][n10][c] > -1) return cost[n5][n10][c];
     if(c >= c_max) return cost[n5][n10][c] = 0;
-
+    if(cost[n5][n10][c] > -1) return cost[n5][n10][c];
     
-    int new_c = get<2>(origin) + 1;
+    
+    if(n5 == 0 and n10 == 0) return (c_max - c)*8;
+    int new_c = c + 1;
     
     lng q = INF;
-    
+    for(int i = 0; i < 4; i++){
+        
+        int new_n1 = n1 + dn1[i];
+        int new_n5 = n5 + dn5[i];
+        int new_n10 = n10 + dn10[i];
+        int w = w_vec[i];
+
+        if(new_n1 >= 0 and new_n5 >= 0 and new_n10 >= 0){
+            State trans = {new_n5, new_n10, new_c};
+            q = min(q, w + solve(trans, new_n1, c_max));
+        }
+    }
 
     cost[n5][n10][c] = q;
 
