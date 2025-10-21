795304098e13ad46c3484afb9d027b6248781125
tomclima <tomecostalima1228@gmail.com>
2025-10-21 17:50:20 -0300

    vector version of raab, still time limit

--- CSES/introductory problems/cses-3399.cpp
+++ CSES/introductory problems/cses-3399.cpp
@@ -8,30 +8,35 @@ using namespace std;
 vector<pair<int, int>> plays;
 
 
-bool backtrack(set<int> &cards_a, set<int> &cards_b, const int rem_a,const  int rem_b){
+bool backtrack(vector<int> &cards, vector<bool> &used_a, vector<bool> &used_b, const int rem_a,const  int rem_b, int rem_cards){
+
     if(rem_a < 0 ) return false;
     if(rem_b < 0) return false;
-    if(rem_a + rem_b > cards_a.size()) return false;    
-    if(rem_a == 0 and rem_b == 0 and cards_a.size() == 0){
+    if(rem_a + rem_b > rem_cards) return false;    
+    if(rem_a == 0 and rem_b == 0 and rem_cards == 0){
         return true;
     }
 
     bool possible = false;
-    for(auto card_a: cards_a){
-        set<int> newcardsa(cards_a);
-        newcardsa.erase(card_a);
-        for(auto card_b : cards_b){
-            set<int> newcardsb(cards_b);
-            newcardsb.erase(card_b);
-            possible = backtrack(newcardsa, newcardsb, rem_a - (card_a > card_b), rem_b - (card_b > card_a));
+    for(int i = 0; i < cards.size(); i++){ // cards_a loop
+        if(used_a[i]) continue;
+        used_a[i] = true;
+        for(int j = 0; j < cards.size(); j++){
+            if(used_b[j]) continue;
+            used_b[j] = true;
+            possible = backtrack(cards, used_a, used_b, rem_a - (cards[i] > cards[j]), rem_b - (cards[j] > cards[i]), rem_cards-1);
+            used_b[j] = false;
+            cout << "a" << endl;
             if(possible) {
-                plays.push_back({card_a, card_b});
+                plays.push_back({cards[i], cards[j]});
                 return true;
             }
 
         }
+        used_a[i] = false;
     }
-    
+        if(rem_a < 0 ) return false;
+
     return false;
 }
 
@@ -44,9 +49,12 @@ int main(){
 
     while(t--){
         int n, a, b; cin >> n >> a >> b;
-        set<int> cards_a, cards_b;
-        for(int i = 1; i <= n; i++) {cards_a.insert(i), cards_b.insert(i);}
-        bool possible =backtrack(cards_a, cards_b, a, b);
+        vector<int> cards;
+        vector<bool> used_a(n, 0);
+        vector<bool> used_b(n, 0);
+        for(int i = 1; i <= n; i++) cards.push_back(i);
+
+        bool possible =backtrack(cards, used_a, used_b, a, b, n);
         if(possible){
             cout << "YES" << endl;
             for(int i = 0; i < plays.size(); i++) cout << plays[i].first << " ";
