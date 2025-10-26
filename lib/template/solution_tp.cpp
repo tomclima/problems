b76abea6a2d4458bf77274c37fc262fc813d3938
tomclima <tomecostalima1228@gmail.com>
2025-10-26 13:57:01 -0300

    new template model

--- lib/template/solution_tp.cpp
+++ lib/template/solution_tp.cpp
@@ -5,7 +5,16 @@ using namespace std;
 #define endl '\n'
 #define ll long long int
 
+int solve(){
+    return 0;
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
 }
