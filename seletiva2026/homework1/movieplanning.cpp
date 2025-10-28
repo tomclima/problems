2b2486c9a8525acfc78ad197eb8749f7c2b376af
tomclima <tomecostalima1228@gmail.com>
2025-10-28 17:26:52 -0300

    I possible solution

--- seletiva2026/homework1/movieplanning.cpp
+++ seletiva2026/homework1/movieplanning.cpp
@@ -5,21 +5,39 @@ using namespace std;
 #define endl '\n'
 #define ll long long int
 
+// pair.first*prefix(m - pair.second +1)
+
+ll mod = 1e9+7;
+
 int solve(){
     int n, m; cin >> n >> m;
 
     vector<pair<int, int>> movies;
     for(int i = 0; i < n; i++){
-        int l, r; cin >> l, r;
+        int l, r; cin >> l >> r;
         movies.push_back({l, r});
     }
-
+    
     auto f = [](auto &a, auto &b){
         return a.first < b.first;
     };
 
-    stable_sort(movies.begin(), movies.end(), f);
-    
+    sort(movies.begin(), movies.end());
+
+    vector<ll> prefix;
+    prefix.push_back(0);
+    for(int i = 1; i <= n; i++){
+        prefix.push_back(((prefix[i-1] + m+ 1 - movies[i-1].second) %mod + mod) %mod);
+    }
+
+    ll qtd = 0;
+    for(int i = 0; i < n; i++){
+        auto index = upper_bound(movies.begin(), movies.end(), make_pair(movies[i].second, 0), f) - movies.begin();
+        
+        qtd = (qtd + ((movies[i].first%mod)*((prefix.back() - prefix[index]) % mod + mod) % mod)%mod)%mod; 
+    }
+
+    cout << qtd << endl;
     return 0;
 }
 
