03c0569d2aa87b4a512ea1254d299ea1e1743222
tomclima <tomecostalima1228@gmail.com>
2026-04-01 12:57:25 -0300

    eu literalmente nunca vou morrer

--- codeforces/Codeforces Round 1070 (Div. 2)/D - Fibonacci Paths.cpp
+++ codeforces/Codeforces Round 1070 (Div. 2)/D - Fibonacci Paths.cpp
@@ -1,33 +1,63 @@
-/*link*/
-
-#include <bits/stdc++.h>
-using namespace std;
-#define endl '\n'
-#define ll long long int
-#define MAXN 1000000000000000000
-
-
-int solve(){
-    int n, m; cin >> n >> m;
-    vector<ll> vertices(n);
-    vector<vector<int>> graph(n);
-    for(ll  &i : vertices) {
-        cin >> i;
+    /*link*/
+     
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define endl '\n'
+    #define ll long long int
+    #define MAXN 1000000000000000000
+     
+    ll mod = (ll)998244353;
+     
+    ll recurse(int i, ll prev, vector<map<ll, ll>> &dp, vector<vector<ll>> &g, vector<ll> &v){
+        if(dp[i].find(prev) != dp[i].end()) return dp[i][prev];
+        if(g[i].size() == 0) return dp[i][prev] = 0;
+     
+        dp[i][prev] = 0;
+        if(prev == -1){
+            for(auto &a : g[i]){
+                dp[i][prev] = (dp[i][prev] + 1 + recurse(a, v[i], dp, g, v)) % mod;
+            }
+        }
+        else{
+            for(auto &a : g[i]){
+                if(v[a] == v[i] + prev){
+                    dp[i][prev] = (dp[i][prev] + 1 + recurse(a, v[i], dp, g, v)) % mod;
+                }
+            }
+        }
+     
+        return dp[i][prev];
     }
-    for(int i = 0; i < m; i++){
-        int a, b; cin >> a>> b;
-        graph[a-1].push_back(b-1);
+     
+     
+    int solve(){
+        int n, m; cin >> n >> m;
+        vector<ll> vertices(n);
+        vector<vector<ll>> graph(n);
+        for(ll  &i : vertices) {
+            cin >> i;
+        }
+        for(int i = 0; i < m; i++){
+            int a, b; cin >> a>> b;
+            graph[a-1].push_back(b-1);
+        }
+     
+        vector<map<ll, ll>> dp(n);
+     
+        ll sum = 0;
+        for(int i = 0; i < n; i++){
+            sum = (sum + recurse(i, -1, dp, graph, vertices)) % mod;
+        }
+     
+        cout << sum << endl;
+        return 0;
     }
-
-
-    return 0;
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int t = 1; cin >> t;
-    while(t--){
-        solve();
+     
+    int main(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        int t = 1; cin >> t;
+        while(t--){
+            solve();
+        }
     }
-}
